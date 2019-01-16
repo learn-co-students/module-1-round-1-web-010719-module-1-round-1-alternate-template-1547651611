fe8930109f4bc1fbdc9ab65f06a0d8b736d0abe3
github username <github email address>
2019-01-16 11:32:39 -0500

    done for now

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,12 +1,53 @@
 class Magazine
-  attr_accessor :name, :category
 
+  @@all = []
+
+  attr_accessor :name, :category
 
   def initialize(name, category)
     @name = name
     @category = category
 
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    Magazine.all.select do |magazine|
+      magazine.name == name
+    end
+  end
+
+  ########
+  # HELPER METHOD
+  def articles
+    Article.all.select do |article|
+      article.magazine == self
+    end
+  end
+
+  def article_titles
+    articles.map do |article|
+      article.title
+    end
+  end
+
+  def contributors
+    #have to go from the magazine through the article join  table to get to the author names
+    binding.pry
+  end
+
+  def most_prolific_author
+
+  end
+
+  def self.most_popular_category
+    binding.pry
+    #have to sort an array of all magazines asc or decs
   end
 
 
-end
+end #end of class
