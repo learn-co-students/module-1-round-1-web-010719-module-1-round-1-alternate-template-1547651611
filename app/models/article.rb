9ea171cc5bb5f4c367a301f20abf222feef93df8
github username <github email address>
2019-01-16 11:30:50 -0500

    done

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,20 @@
 class Article
 
+  attr_reader :author, :title, :magazine
 
-end
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize(author, title, magazine)
+    @author = author
+    @title = title
+    @magazine = magazine
+
+    @@all << self
+  end
+
+
+end #end of class
--- tools/console.rb
+++ tools/console.rb
@@ -8,6 +8,30 @@ end
 ###  WRITE YOUR TEST CODE HERE ###
 
 
+nyp = Magazine.new("New York Press", "politics")
+
+tdm = Magazine.new("The Daily Mail", "politics")
+
+si = Magazine.new("Sports Illustrated", "sports")
+
+zev = Author.new("Zev")
+
+someone = Author.new("someone")
+
+cats = Article.new(zev, "cats", nyp)
+
+feet = Article.new(zev, "feet", nyp)
+
+cup = Article.new(someone, "cup", nyp)
+
+dogs= Article.new(zev, "dogs", si)
+
+mice = Article.new(zev, "mice", tdm)
+
+
+
+
+
 
 
 
