aab1b4192bbcc791676dee425d46d30ab72518ad
cannoli9917 <katie.pennachio@gmail.com>
2019-01-16 11:19:49 -0500

    I think this is everything

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,11 +1,40 @@
 class Author
-  attr_accessor :name
+  attr_reader :name
 
+  @@all = []
+
+  def self.all
+    @@all
+  end
 
   def initialize(name)
     @name = name
-  
+
+    @@all << self
+
+  end
+
+  def add_article(title,magazine)
+    Article.new(title,self,magazine)
+  end
+
+  def articles
+    Article.all.select do |article|
+      article.author == self
+    end
+  end
+
+  def magazines
+    articles.map do |article|
+      article.magazine
+    end
+  end
+
+  def show_specialties
+    magazines.map do |mag|
+      mag.category
+    end
   end
 
 
-end
+end #end of Author class
