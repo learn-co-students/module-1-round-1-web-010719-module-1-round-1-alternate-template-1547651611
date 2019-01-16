c4c099cf1401bda5d17085d4817db5246cdf4030
github username <github email address>
2019-01-16 11:32:29 -0500

    first round

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,16 @@
 class Article
 
+  attr_reader :author, :magazine
+  @@all = []
+
+  def initialize(author, magazine)
+    @author = author
+    @magazine = magazine
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 
 end
