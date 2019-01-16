ab72ce75520529c610bf2867a5470dbe2f47cb2c
purplesands <mullenjoe13@gmail.com>
2019-01-16 11:30:36 -0500

    coding challenge submission

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,12 +1,40 @@
 class Magazine
+
   attr_accessor :name, :category
 
+  @@all = []
 
   def initialize(name, category)
     @name = name
     @category = category
-
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
 
-end
+  def self.find_by_name(name)
+    Magazine.all.find {|magazine| magazine.name == name}
+  end
+
+  def article_titles
+    Article.all.select {|article| article.magazine == self}
+  end
+
+  def contributors
+    article_titles.map {|article| article.author}
+  end
+
+  def most_prolific_author
+    article_titles.max_by {|article| article.author.length}
+    ##does not work! was trying to get each author into individual arrays of articles they had written, then compare length and output the longest one (most articles).
+  end
+
+  def self.most_popular_category
+    Magazine.all.sort_by{|magazine| magazine.category.length}.reverse
+  end
+
+  ## also does not work... trying to do a similar thing as above, but is sorting alphabetically rather than by occurrence. feel confident I could figure this out with a bit more time but ran out!
+
+end ## END OF MAGAZINE CLASS
--- tools/console.rb
+++ tools/console.rb
@@ -4,8 +4,26 @@ def reload
   load 'config/environment.rb'
 end
 
-
 ###  WRITE YOUR TEST CODE HERE ###
+
+m1 = Magazine.new("cool magazine", "cool category")
+m2 = Magazine.new("dumb magazine", "dumb category")
+m3 = Magazine.new("untitled magazine", "dumb category")
+m4 = Magazine.new("untitled magazine 2", "untitled category")
+
+
+au1 = Author.new("bob")
+au2 = Author.new("steve")
+
+ar1 = Article.new("cool article", au1, m1)
+
+au1.add_article("about carrots", m2)
+au1.add_article("about celery", m2)
+au2.add_article("about yams", m1)
+au2.add_article("about soup", m1)
+au2.add_article("about vegetables in general", m1)
+
+
 
 
 
